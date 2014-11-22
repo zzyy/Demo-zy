10afabc72f24f3011d3bff470b544438fdf8fff9
zouyun <zou.yun@eastaeon.com>
2014-11-22 13:21:36 +0800

    2014-11-22 13:21:34

--- DrawFlow/src/main/java/com/zy/drawflow/app/MainActivity.java
+++ DrawFlow/src/main/java/com/zy/drawflow/app/MainActivity.java
@@ -2,35 +2,24 @@ package com.zy.drawflow.app;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
+    private static String TAG = "zy.MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    protected void onResume() {
+        Log.d(TAG, "onResume");
+        super.onResume();
     }
 }
